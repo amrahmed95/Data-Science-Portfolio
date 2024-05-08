191a4209a9dcfffcd94967343e5fb0055fbaf8c2
Amr Ahmed <amr.ahmedm95@gmail.com>
2024-05-08 21:43:25 +0300

    Add TechPath Navigator - Initial dir

---
+++ End-to-End/TechPath Navigator/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='TechPath Navigator is a data-driven platform that helps IT students and professionals discover the skills and technologies linked to various technology-related careers',
+    author='Amr Ahmed',
+    license='MIT',
+)
